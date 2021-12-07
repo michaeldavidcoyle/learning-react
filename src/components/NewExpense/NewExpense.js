3c821c40f22ea15e3ba1704f1e12f1e9b95690b3
Michael Coyle <michael.david.coyle4@gmail.com>
2021-12-06 20:48:57 -0600

    Using props and handler functions to pass expense data from the form to App.js

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,21 @@
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    // yields a random ID, a string, of 7 alphanumeric characters (7 digit number in base 36)
+    const randomID = () => Math.floor(Math.random() * 7.8e10).toString(36).padStart(7, '0');
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: randomID()
+        }
+
+        props.onAddExpense(expenseData);
+    }
+
     return <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
 };
 
